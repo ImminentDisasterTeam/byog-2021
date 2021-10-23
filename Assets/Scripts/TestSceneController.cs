4b529042ce1a970b2f4da843eae91f6bb20c0ba4
AntonyMo <32247865+AntonyMoes@users.noreply.github.com>
2021-10-23 18:40:37 +0300

    Add level selection and loading animation

--- Assets/Scripts/TestSceneController.cs
+++ Assets/Scripts/TestSceneController.cs
@@ -2,10 +2,7 @@
 using UnityEngine;
 
 public class TestSceneController : MonoBehaviour {
-    [SerializeField] private TextAsset _levelText;
-
     private void Start() {
-        LevelController.Instance.StartLevel(_levelText.text, 1);
-        UIController.Instance.ShowLevelUI();
+        UIController.Instance.ShowMainMenu(null, true);
     }
 }
