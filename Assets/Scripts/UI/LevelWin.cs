e918bef9eb3e4872f06bd817f68b7129cfbab511
AntonyMo <32247865+AntonyMoes@users.noreply.github.com>
2021-10-23 19:21:47 +0300

    Add LevelWin script

---
+++ Assets/Scripts/UI/LevelWin.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+namespace UI {
+    public class LevelWin : Window {
+        [SerializeField] private UnityEngine.UI.Button _nextLevelButton;
+        [SerializeField] private UnityEngine.UI.Button _exitButton;
+    }
+}
